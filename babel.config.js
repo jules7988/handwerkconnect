5bb3cbebd78c2384e595183b950be6ff7cab462a
jules7988 <julian.schaeble@gmail.com>
2025-10-06 10:45:51 +0200

    fix(build): add babel preset & babel.config; update package.json and ignore

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-worklets/plugin'],
+    plugins: [
+      // nur wenn expo-router im Projekt ist (empfohlen):
+      'expo-router/babel',
+      // falls du react-native-reanimated verwendest, hänge das *als letztes* Plugin an:
+      // 'react-native-reanimated/plugin',
+    ],
   };
 };
